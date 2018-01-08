018f34fa4c070d19eb4499127cbf549738b2f684
Jammy Zhou <jammy.zhou@gmail.com>
2018-01-08 17:07:53 +0800

    Add deploy_resnet_rpc.py
    Still some error below when deploy resnet to Bubblegum96 with RPC
    
    tvm._ffi.base.TVMError: Except caught from RPC call:
    src/runtime/module_util.cc:46: Check failed: ret == 0 (-1 vs. 0)
    src/runtime/module.cc:99: Check failed: f != nullptr Cannot find function
    fuse_conv2d_broadcast_mul_broadcast_add_relu__kernel0 in the imported
    modules or global registry

---
+++ deploy_resnet_rpc.py
@@ -0,0 +1,77 @@
+import mxnet as mx
+import nnvm
+import tvm
+import numpy as np
+
+# Use "Agg" backend of matplotlib to make it work with ssh
+import matplotlib
+matplotlib.use('Agg')
+
+from mxnet.gluon.model_zoo.vision import get_model
+from mxnet.gluon.utils import download
+from PIL import Image
+from matplotlib import pyplot as plt
+from tvm.contrib import util, rpc
+
+block = get_model('resnet18_v1', pretrained=True)
+img_name = 'cat.jpg'
+synset_url = ''.join(['https://gist.githubusercontent.com/zhreshold/',
+                      '4d0b62f3d01426887599d4f7ede23ee5/raw/',
+                      '596b27d23537e5a1b5751d2b0481ef172f58b539/',
+                      'imagenet1000_clsid_to_human.txt'])
+synset_name = 'synset.txt'
+download('https://github.com/dmlc/mxnet.js/blob/master/data/cat.png?raw=true', img_name)
+download(synset_url, synset_name)
+with open(synset_name) as f:
+    synset = eval(f.read())
+image = Image.open(img_name).resize((224, 224))
+plt.imshow(image)
+plt.show()
+
+def transform_image(image):
+    image = np.array(image) - np.array([123., 117., 104.])
+    image /= np.array([58.395, 57.12, 57.375])
+    image = image.transpose((2, 0, 1))
+    image = image[np.newaxis, :]
+    return image
+
+x = transform_image(image)
+print('x', x.shape)
+
+sym, params = nnvm.frontend.from_mxnet(block)
+# we want a probability so add a softmax operator
+sym = nnvm.sym.softmax(sym)
+
+import nnvm.compiler
+target = 'opencl'
+target_host = 'llvm -target=aarch64-linux-gnu -mcpu=cortex-a53 -mattr=+neon'
+shape_dict = {'data': x.shape}
+graph, lib, params = nnvm.compiler.build(sym, target, shape_dict, params=params, target_host=target_host)
+
+# Save the library at local temporary directory.
+tmp = util.tempdir()
+lib_fname = tmp.relpath('sym.o')
+lib.save(lib_fname)
+
+# connect the server
+remote = rpc.connect('192.168.1.14', 9090)
+
+# upload the library to remote device and load it
+remote.upload(lib_fname)
+rlib = remote.load_module('sym.o')
+
+from tvm.contrib import graph_runtime
+ctx = remote.cl(0)
+# upload the parameter
+rparams = {k: tvm.nd.array(v, ctx) for k, v in params.items()}
+dtype = 'float32'
+m = graph_runtime.create(graph, rlib, ctx)
+# set inputs
+m.set_input('data', tvm.nd.array(x.astype(dtype)))
+m.set_input(**rparams)
+# execute
+m.run()
+# get outputs
+tvm_output = m.get_output(0, tvm.nd.empty((1000,), dtype))
+top1 = np.argmax(tvm_output.asnumpy())
+print('TVM prediction top-1:', top1, synset[top1])
